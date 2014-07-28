7a1284c98bad441e7ffe44c03b980277de2fac83
Simon Deng <simonsdeng@gmail.com>
2014-07-27 23:57:37 -0700

    Use initializer lists instead of assignment

--- 22B-Project/Book.cpp
+++ 22B-Project/Book.cpp
@@ -6,13 +6,12 @@
 using namespace std;
 
 Book::Book(long isbn, string title, string author, string publisher, double cost,
-			double price, int qty, time_t date) {
-	this->isbn = isbn;
-	this->title = title;
-	this->author = author;
-	this->publisher = publisher;
-	this->date = date;
-	this->qty = qty;
-	this->cost = cost;
-	this->price = price;
-}
+			double price, int qty, time_t date)
+	: isbn(isbn)
+	, title(title)
+	, author(author)
+	, publisher(publisher)
+	, date(date)
+	, qty(qty)
+	, cost(cost)
+	, price(price) {}
