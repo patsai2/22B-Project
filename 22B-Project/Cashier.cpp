12811d499dad7608adf1be670fb9ff5e46f90f19
mlm22B <monicametzger@Monicas-MacBook-Pro-3.local>
2014-07-31 13:53:30 -0700

    Cout and Cin info

--- 22B-Project/Cashier.cpp
+++ 22B-Project/Cashier.cpp
@@ -8,7 +8,7 @@
 
 #include "Cashier.h"
 #include <iostream>
-//#include "stdio.h"
+#include "stdio.h"
 #include "Inventory.h"
 
 
@@ -18,22 +18,28 @@ using namespace std;
 Cashier::Cashier(Inventory &inventory): inventory(inventory)
 {
 
+    
 }
 
 void Cashier::run()
 {
     string booktitle;           //book title
     int x;                      //quantity of books
+    
+    
     cout << "Serendipity Book Sellers" <<endl;
     cout << endl;
-    //cout << "Date:"<<" "<< __DATE__ << endl;
-    //cout << "Time:"<< " "<< __TIME__ << endl;
+    cout << "Date:"<<" "<< __DATE__ << endl;
+    cout << "Time:"<< " "<< __TIME__ << endl;
+    
     cout << endl;
     cout<< "Which Book would you like purchase?" << endl;
     cout << endl;
 	cout << "Books: " << endl;
 	
-    vector<const Book*> books = getBooks();
+    vector<const Book*> books = getBooks();     //figure out vectors
+
+    
     for (size_t i = 0; i < books.size(); i++)
     {
 		cout << i + 1 << ". " << books[i]->qty << " x "
@@ -45,6 +51,21 @@ void Cashier::run()
     cin >> booktitle;
     cout<<"How many books with that title would you like to purchase?" << endl;
     cin>>x;
+    
+    remove(books[input-1], qty);
+    
+     cout << "Serendipity Book Sellers" <<endl;
+     cout << endl;
+     cout << "Date:"<<" "<< __DATE__ << endl;
+     cout<<"Qty     ISBN            Title           Price         Total" <<endl;
+     cout<<"-----------------------------------------------------------"<<endl;
+    cout<<"     "<< x<< isbn<< title<< Price<< total<endl;     //print out info they need
+    cout<< endl<<endl;
+    cout<<"Subtotal:"<<" "<< /*print out subtitle*/ << endl;
+    cout<<"Tax:"<<" "<< /*print out tax*/ << endl;
+    cout<<"Total:"<<" "<< /*print out total*/ << endl;
+    cout<<endl;
+    cout<< "Thanks For Shopping at Serendipity."
 }
 
 
